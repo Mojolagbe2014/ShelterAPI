eb535182492136aa96fe52abcd2d53e3d582b26d
jamiumojolagbe <jamiumojolagbe@192.168.0.70>
2018-05-21 12:44:39 -0500

    updates

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // api general config
 var config = {
-    env: "production",
-    user: "b2c4d9376f7c46",
-    password: "8593db63",
-    database: "heroku_6c3715b17185b6c",
-    host: "us-cdbr-iron-east-04.cleardb.net",
-    port: 3306,
+    env: "development",
+    user: "root",
+    password: "mysql",
+    database: "pets",
+    host: "localhost",
+    port: 8000,
     client: "mysql",
     charset: "utf8",
     migrations: "knex_migrations",

7133da0a6011cd6a0db2d2838cd277d49ee95bba
jamiumojolagbe <jamiumojolagbe@192.168.0.70>
2018-05-21 11:12:55 -0500

    updates

--- knex.js
+++ knex.js
@@ -1,16 +1,9 @@
 // api general config
 var config = require('./config');
 
-const Knex = require('knex')( {
-    client: config.client,
-    connection: {
-        host: config.host,
-        user: config.user,
-        password: config.password,
-        database: config.database,
-        charset: config.charset
-    }
-
-} );
+var dbconfig    = require('./knexfile'); 
+var Knex        = require('knex')(dbconfig[config.env]);
 
 module.exports = Knex;
+
+Knex.migrate.latest([dbconfig]); 

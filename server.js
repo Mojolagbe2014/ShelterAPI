f8d297931598c7bcbec10fce8bc455ea28ae44c9
jamiumojolagbe <jamiumojolagbe@192.168.0.70>
2018-05-21 11:37:40 -0500

    updates

--- server.js
+++ server.js
@@ -109,6 +109,48 @@ server.route({
     }
 });
 
+// Create: Add a Pet
+server.route({
+    path: '/pets/',
+    method: 'POST',
+    handler: (request, response) => {
+        const pet = request.payload;  // formdata or json
+        const create = Knex('pets').insert({
+            name: pet.name,
+            type: pet.type,
+            breed: pet.breed,
+            location: pet.location,
+            latitude: pet.latitude,
+            longitude: pet.longitude,
+            //picture_url: pet.picture_url,
+            status: true
+        }).then(result => {
+            response({
+                status: 1,
+                message: `Pet (${pet.name}) successfully added.`
+            });
+        }).catch(error => {
+            response({
+                status: 0,
+                message: `Server-Side Error >> ${error}`
+            });
+        });
+    },
+    config: {
+        validate: {
+            payload: {
+                name: Joi.string().alphanum().min(3).max(100).required(),
+                type: Joi.string().min(3).max(100).required(),
+                breed: Joi.string().min(3).max(100).required(),
+                location: Joi.string().min(3).max(100).required(),
+                latitude: Joi.number().min(-90).max(90).required(),
+                longitude: Joi.number().min(-180).max(180).required()
+                //picture_url: Joi.string().uri().trim().required()
+            }
+        }
+    }
+});
+
 
 
 // ------------------

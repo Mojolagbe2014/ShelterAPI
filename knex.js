172e6bace6b90d7024706d40f4749860b3c9f1b8
jamiumojolagbe <jamiumojolagbe@192.168.0.70>
2018-05-21 11:42:49 -0500

    updates

--- knex.js
+++ knex.js
@@ -6,6 +6,6 @@ var Knex        = require('knex')(dbconfig[config.env]);
 
 module.exports = Knex;
 
-Knex.migrate.rollback([dbconfig]);
-//Knex.migrate.latest([dbconfig]); 
-//Knex.seed.run([dbconfig]);
+//Knex.migrate.rollback([dbconfig]);
+Knex.migrate.latest([dbconfig]); 
+Knex.seed.run([dbconfig]);
